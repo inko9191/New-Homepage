6c246cea28d773b27d8948758d263a076d585cc4
平方祐有 <hirakatayuuyuu@23125birdhead.local>
2026-04-18 23:29:56 +0900

    Soften mobile assembly intel panel

--- division.js
+++ division.js
@@ -7,7 +7,7 @@
 
     const qs = (selector, root = document) => root.querySelector(selector);
     const qsa = (selector, root = document) => Array.from(root.querySelectorAll(selector));
-    const APP_VERSION = '20260418-2356';
+    const APP_VERSION = '20260418-2363';
 
     const toAbsolutePageUrl = (rawHref) => {
         if (!rawHref) return rawHref;
--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 (() => {
     'use strict';
 
-    const APP_VERSION = '20260418-2356';
+    const APP_VERSION = '20260418-2363';
 
     const toAbsolutePageUrl = (rawHref) => {
         if (!rawHref) return rawHref;
